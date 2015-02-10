c7951c7c2aca3718349f067dde0d8ad745712bd3
Patrick Double <pat@patdouble.com>
2015-02-10 05:26:41 -0600

    Examples for ios-driver.github.io

---
+++ src/functionalTest/java/ebay.java
@@ -0,0 +1,22 @@
+/*
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.uiautomation.ios.IOSCapabilities;
+*/
+import java.net.URL;
+
+public class ebay {
+    public static void main(String[] args) throws Exception {
+        /*
+        DesiredCapabilities safari = IOSCapabilities.iphone("Safari");
+        System.out.println(safari);
+        RemoteWebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), safari);
+
+        driver.get("http://www.ebay.com/");
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+        */
+    }
+}
